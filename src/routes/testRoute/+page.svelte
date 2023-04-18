6e9dfe4d4beff54a3161e6f988ff4bd4f026a90a
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-04-18 08:47:10 +0000

    Initial routing inside svelte

---
+++ src/routes/testRoute/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to TestRoute</h1>
+<a href="/" style="margin-right: 0.5rem;">Go to main page</a>
+<a href="/testRoute/BeneathTestRoute">Go Deeper!</a>
---
+++ src/routes/testRoute/BeneathTestRoute/+page.svelte
@@ -0,0 +1,3 @@
+<p>Welcome to the deeper Test Route! Go Back</p>
+<a href="/testRoute" style="margin-right: 0.5rem;">Go Back</a>
+<a href="/">Go to The Main Page</a>
