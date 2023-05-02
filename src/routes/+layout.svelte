dc82615bcb85861c7a80dd7e082fa2f8e966ed8f
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-05-02 08:15:29 +0000

    Setting Up Fonts and Background Color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,35 +9,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
 
-<div class="main-wrapper">
-	<div class="sidebar" />
-	<div class="content">
-		<slot />
-	</div>
-</div>
+<slot />
 
 <style>
 	:global(*) {
 		margin: 0;
 		padding: 0;
+        font-family: "Open Sans", sans-serif;
 	}
 	:global(html, body) {
 		width: 100%;
 		height: 100%;
-	}
-	.main-wrapper {
-		width: 100%;
-		height: 100%;
-		display: flex;
-		flex-direction: row;
-	}
-	.sidebar {
-		background-color: blue;
-		width: 200px;
-		height: 100%;
-	}
-	.content {
-		display: flex;
-		flex-direction: column;
+        background-color: #fffde7;
 	}
 </style>
