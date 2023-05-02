aebd2f0f5547cd35adc985c12298a147d2b1d260
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-05-02 07:57:33 +0000

    Layouting Sidebar on Main Page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<div class="main-wrapper">
+    <div class="sidebar"></div>
+    <div class="content">
+        <slot/>
+    </div>
+</div>
+
+<style>
+    :global(*) {
+        margin: 0;
+        padding: 0;
+    }
+    :global(html, body) {
+        width: 100%;
+        height: 100%;
+    }
+    .main-wrapper {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+    }
+    .sidebar {
+        background-color: blue;
+        width: 200px;
+        height: 100%;
+    }
+    .content {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
