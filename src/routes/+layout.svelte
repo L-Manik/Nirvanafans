217e7e5fa4157ea4c4a599980cea28c4c3a9d109
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-05-17 14:13:40 +0000

    Adding Title to website tab

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 		href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Peralta&display=swap"
 		rel="stylesheet"
 	/>
+	<title>Svelte Spongebob CV</title>
 	<script src="https://kit.fontawesome.com/5bb4fcfd58.js" crossorigin="anonymous"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
