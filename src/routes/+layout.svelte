096ef2548ddf63681e3bc0737ff0416472dab517
L-Manik <124031190+L-Manik@users.noreply.github.com>
2023-05-30 14:12:43 +0000

    Main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,11 +16,17 @@
 	:global(*) {
 		margin: 0;
 		padding: 0;
-        font-family: "Open Sans", sans-serif;
+		font-family: 'Open Sans';
+		color: #e3e5f2;
+		box-sizing: border-box;
 	}
 	:global(html, body) {
 		width: 100%;
 		height: 100%;
-        background-color: #fffde7;
+		background: linear-gradient(45deg, #101223, #09104d);
 	}
+	:global(a) {
+		text-decoration: none;
+	}
+
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,195 +1,76 @@
 <script lang="ts">
+	import Section from '$components/Section.svelte';
 	import book from 'svelte-awesome/icons/book';
+	import music from 'svelte-awesome/icons/music';
+	import cogs from 'svelte-awesome/icons/cogs';
 	import user from 'svelte-awesome/icons/user';
-	import briefcase from 'svelte-awesome/icons/briefcase';
-	import cogs from "svelte-awesome/icons/cogs";
-	import Section from "$components/Section.svelte";
 </script>
 
-<!-- opening part -->
-<div class="readable-area">
-	<a class="header" href="./testRoute">
-		<div class="header-text">
-			<h1 class="name">Squarepants, Spongebob</h1>
-			<p class="role">Head Chef</p>
+<head>
+	<meta charset="utf-8" />
+</head>
+
+<div class="wrapper">
+	<div class="header">
+		<div class="logo">
+			<img
+				src="https://1000logos.net/wp-content/uploads/2017/03/Nirvana-Logo-1989.png"
+				alt="logo"
+				class="brandlogo"
+			/>
 		</div>
+		<div class="more" />
+		<a href="/membersRoute"class="redirects">Members</a>
+		<div class="redirects">Albums</div>
+		<div class="redirects">Merch</div>
+	</div>
+	<div class="slideshow">
 		<img
-			src="https://assets.nick.com/uri/mgid:arc:imageassetref:nick.com:a625d441-bbbf-42c8-9927-6a0157aac911?quality=0.7"
-			alt="Spongebob Profile"
+			src="https://cdn.fansshare.com/images/nirvana/nirvana-live-1337557331.jpg"
+			alt="livephoto1"
+			class="livephoto"
 		/>
-	</a>
-	<!-- first container -->
-	<div class="content">
-		<Section iconData={user} title="Profile">
-				<p>LOREM IPSUN FOR THE WIN</p>
-		</Section>
-		<Section iconData={book} title="Education">
-				<ul>
-				<li>
-					<span>2011 - 2014</span>
-					<span class="place">Bikini Bottom High School</span>
-				</li>
-				<li>
-					<span>2014 - 2016</span>
-					<span class="place">McGuy University</span>
-				</li>
-				</ul>
-		</Section>
-		<Section iconData={briefcase} title="Job Experience">
-				<ul>
-					<li>
-						<span>Burger Flipper (2017 - 2018)</span>
-						<span class="place">Krusty Wrab</span>
-					</li>
-					<li>
-						<span>Ministry of Defense (2018 - 2020)</span>
-						<span class="place">Chum Bucket</span>
-					</li>
-				</ul>
-		</Section>
-		<Section iconData={cogs} title="Skills">
-				<ul>
-					<li class="skill">
-						<span class="skill-title">Jellyfish Hunting</span>
-						<div class="gauge">
-							<div class="bullet filled-bullet" />
-							<div class="bullet filled-bullet" />
-							<div class="bullet filled-bullet" />
-							<div class="bullet" />
-							<div class="bullet" />
-						</div>
-					</li>
-					<li class="skill">
-						<span class="skill-title">Whale Singing</span>
-						<div class="gauge">
-							<div class="bullet filled-bullet" />
-							<div class="bullet filled-bullet" />
-							<div class="bullet" />
-							<div class="bullet" />
-							<div class="bullet" />
-						</div>
-					</li>
-					<li class="skill">
-						<span class="skill-title">Burger Flipping</span>
-						<div class="gauge">
-							<div class="bullet filled-bullet" />
-							<div class="bullet filled-bullet" />
-							<div class="bullet filled-bullet" />
-							<div class="bullet filled-bullet" />
-							<div class="bullet filled-bullet" />
-						</div>
-					</li>
-				</ul>
-		</Section>
 	</div>
 </div>
 
 <style>
-	.readable-area {
-		display: flex;
-		flex-direction: column;
-		background-color: #ffffff;
-		margin: 0 5%;
-		height: 100%;
-	}
-    
-	.name {
-		font-family: 'Peralta', cursive;
-		color: #000;
-	}
-
-	.role {
-		font-weight: 500;
-		color: #000;
-	}
-
-	img {
-		width: 128px;
-		height: 128px;
-		border-radius: 100%;
-		border: 4px solid #5e35b1;
+	.wrapper {
+		overflow-x: hidden;
 	}
 
 	.header {
 		display: flex;
-		flex-direction: column-reverse;
-		padding: 16px;
-		justify-content: end;
-		align-items: center;
-		border-bottom: 4px solid #000000;
-		text-decoration: none;
+		flex-direction: columns;
+		margin-right: 2rem;
 	}
 
-	.header-text {
-		text-align: center;
-		margin-right: 16px;
-	}
-
-	/* container */
-	.content {
-		display: grid;
-		grid-template-columns: repeat(1, minmax(0, 1fr));
-		gap: 2rem;
-		padding: 2rem;
-	}
-
-	ul {
-		list-style-type: none;
-	}
-
-	li {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.place {
-		margin-left: 16px;
-	}
-
-	.skill-title {
+	.logo {
 		flex: 1;
+		padding: 1rem;
 	}
 
-	.gauge {
+	.brandlogo {
+		max-width: 100px;
+	}
+
+	.more {
 		display: flex;
-		flex-direction: row;
-		margin-left: 8px;
+		flex-direction: columns;
 	}
 
-	.bullet {
-		width: 16px;
-		height: 16px;
-		border-radius: 100%;
-		margin: 4px;
-		background-color: #fff59d;
+	.redirects {
+		margin: 1rem 1rem;
 	}
 
-	.filled-bullet {
-		background-color: #fbc02d;
+	.redirects:hover {
+		border-bottom: solid 3px white;
+        transform:scale(1.1)
 	}
 
-	.skill {
-		flex-direction: row;
-		align-items: center;
-		margin-bottom: 4px;
-	}
-
-	/* mobile first approach for future where there are more people using their phones */
-	@media (min-width: 600px) {
-		.readable-area {
-			margin: 0 20%;
-		}
-
-		.header {
-			flex-direction: row;
-		}
-
-		.header-text {
-			text-align: end;
-		}
-
-		.content {
-			grid-template-columns: repeat(2, minmax(0, 1fr))
-		}
-	}
+    .livephoto {
+        display:flex;
+        justify-content: center;
+        margin-top: 3rem;
+		width: 100vw;
+    }
 </style>
