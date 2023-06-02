96a2b6bbf7c1d70ceeeb8e6b42681d812e27f00d
L-Manik <124031190+L-Manik@users.noreply.github.com>
2023-06-02 08:16:56 +0000

    Finished adding merch on merch page

--- src/routes/merchRoute/+page.svelte
+++ src/routes/merchRoute/+page.svelte
@@ -6,42 +6,36 @@
 			class="brandlogo"
 		/>
 		<h1>Merchandise</h1>
+		<a href="/">Return</a>
 	</div>
 
 	<div class="content">
 		<div class="merchlist">
 			<div class="merch">
-                <img src="https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406" alt="hoodie">
                 <p>Gritty Nevermind Hoodie</p>
             </div> 
-            <div class="merch">
-                <img src="https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406" alt="hoodie">
-                <p>Gritty Nevermind Hoodie</p>
+			<div class="merch">
+                <p>Drain You Crewneck Sweatshirt</p>
             </div> 
-            <div class="merch">
-                <img src="https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406" alt="hoodie">
-                <p>Gritty Nevermind Hoodie</p>
+			<div class="merch">
+                <p>Abstract Water Crewneck Sweatshirt</p>
             </div> 
-            <div class="merch">
-                <img src="https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406" alt="hoodie">
-                <p>Gritty Nevermind Hoodie</p>
+			<div class="merch">
+                <p>Drain You Zip Hoodie</p>
             </div> 
-            <div class="merch">
-                <img src="https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406" alt="hoodie">
-                <p>Gritty Nevermind Hoodie</p>
+			<div class="merch">
+                <p>Dim Light Portrait Tee</p>
             </div> 
-            <div class="merch">
-                <img src="https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406" alt="hoodie">
-                <p>Gritty Nevermind Hoodie</p>
+			<div class="merch">
+                <p>I'm So Happy Smiley Tee</p>
             </div> 
-            <div class="merch">
-                <img src="https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406" alt="hoodie">
-                <p>Gritty Nevermind Hoodie</p>
+			<div class="merch">
+                <p>GLive at Reading Black Tee</p>
             </div> 
-            <div class="merch">
-                <img src="https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406" alt="hoodie">
-                <p>Gritty Nevermind Hoodie</p>
+			<div class="merch">
+                <p>Sliver Tee - White</p>
             </div> 
+
 		</div>
 	</div>
 </div>
@@ -80,7 +74,6 @@
 
 	.merchlist {
 		padding: 1em;
-		border: 5px grey solid;
 		display: flex;
 		flex-direction: row;
         flex-wrap: wrap;
@@ -88,15 +81,125 @@
 
 	.merch {
         margin: 1rem;
-        text-align: center;
+		display: flex;
+		align-items: flex-end;
+		justify-content: center;
+        text-align: bottom;
         font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 		border: 5px #000000 solid;
 		border-radius: 15px;
+		width: 320px;
 	}
 
-    .merch img {
-        max-width: 320px;
-		margin-bottom: 1rem;
-		margin-top: 1rem;
-    }
+	.merch:nth-child(1) {
+		width:320px;
+		height:352px;
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRGRITHOODIE.png?v=1632378406');
+		background-size: 100%;
+		background-repeat:no-repeat;
+		transition: 0.5s;
+	}
+
+	.merch:nth-child(1):hover {
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRHOODIE2.png?v=1632378406');
+		transform: scale(1.1)
+	}
+
+	.merch:nth-child(2) {
+		width:320px;
+		height:352px;
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRDRAIN3.png?v=1632378113');
+		background-size: 100%;
+		background-repeat:no-repeat;
+		transition: 0.5s;
+	}
+
+	.merch:nth-child(2):hover {
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRDRAIN1.png?v=1632378113');
+		transform: scale(1.1)
+	}
+
+	.merch:nth-child(3) {
+		width:320px;
+		height:352px;
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRBLUESWEATSHIRT.png?v=1632377629');
+		background-size: 100%;
+		background-repeat:no-repeat;
+		transition: 0.5s;
+	}
+
+	.merch:nth-child(3):hover {
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRSWEATSHIRT2.png?v=1632377629');
+		transform: scale(1.1)
+	}
+
+	.merch:nth-child(4) {
+		width:320px;
+		height:352px;
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRZIP.png?v=1632378251');
+		background-size: 100%;
+		background-repeat:no-repeat;
+		transition: 0.5s;
+	}
+
+	.merch:nth-child(4):hover {
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRZIP3.png?v=1632378251');
+		transform: scale(1.1)
+	}
+
+	.merch:nth-child(5) {
+		width:320px;
+		height:352px;
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRZIP.png?v=1632378251');
+		background-size: 100%;
+		background-repeat:no-repeat;
+		transition: 0.5s;
+	}
+
+	.merch:nth-child(5):hover {
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRZIP3.png?v=1632378251');
+		transform: scale(1.1)
+	}
+
+	.merch:nth-child(6) {
+		width:320px;
+		height:352px;
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRHAPPYTEE.png?v=1613596878');
+		background-size: 100%;
+		background-repeat:no-repeat;
+		transition: 0.5s;
+	}
+
+	.merch:nth-child(6):hover {
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRHAPPYBACKTWW.png?v=1613597529');
+		transform: scale(1.1)
+	}
+
+	.merch:nth-child(7) {
+		width:320px;
+		height:352px;
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRliveatreadingtee.png?v=1596652330');
+		background-size: 100%;
+		background-repeat:no-repeat;
+		transition: 0.5s;
+	}
+
+	.merch:nth-child(7):hover {
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRliveatreadingteefront.png?v=1596652330');
+		transform: scale(1.1)
+	}
+
+	.merch:nth-child(8) {
+		width:320px;
+		height:352px;
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRSLIVERWHITETEE.png?v=1601004158');
+		background-size: 100%;
+		background-repeat:no-repeat;
+		transition: 0.5s;
+	}
+
+	.merch:nth-child(8):hover {
+		background-image: url('https://cdn.shopify.com/s/files/1/2081/4611/products/NIRsliverwhitefront.png?v=1601004158');
+		transform: scale(1.1)
+	}
 </style>
